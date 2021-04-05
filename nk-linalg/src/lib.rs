a860e388b423a09b1b984bacad6bf4d0fd545114
noah <nomaxx117@gmail.com>
2021-04-05 14:48:10 -0500

    Adding mul

--- nk-linalg/src/lib.rs
+++ nk-linalg/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Mul;
+
 pub struct Matrix<T, const R: usize, const C: usize> {
     inner: [[T; R]; C]
 }
@@ -5,3 +7,11 @@ pub struct Matrix<T, const R: usize, const C: usize> {
 pub type RVec<T, const N: usize> = Matrix<T, 1, N>;
 
 pub type CVec<T, const N: usize> = Matrix<T, N, 1>;
+
+impl <T, const R: usize, const C: usize, const N: usize> Mul<Matrix<T, N, C>> for Matrix<T, R, N> {
+    type Output = Matrix<T, R, C>;
+
+    fn mul(self, rhs: Matrix<T, N, C>) -> Self::Output {
+        todo!()
+    }
+}
